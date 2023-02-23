d066871050147121de5eebb6989d34be3b33e8fc
Román Solanas <72008596+RomanCito-969@users.noreply.github.com>
2023-02-23 13:13:53 +0100

    Servidor Eureka

---
+++ server_eureka/src/test/java/fp/dual/server_eureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package fp.dual.server_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
