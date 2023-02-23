d16c4f57b9e9bd031f6b869e18c8e415e75921a5
Román Solanas <72008596+RomanCito-969@users.noreply.github.com>
2023-02-23 13:19:52 +0100

    API GATEWAY
    Incompleto

---
+++ gateway/src/test/java/fp/dual/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package fp.dual.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
